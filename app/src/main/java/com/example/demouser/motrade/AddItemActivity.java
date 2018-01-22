1cb8e41105e323db47ce07c99511dd2e0305c8df
Briar Harrison <harri25j@mtholyoke.edu>
2018-01-22 10:56:32 -0500

    Adding basic camera functionality - take a picture and display it in the add item activity.

--- app/src/main/java/com/example/demouser/motrade/AddItemActivity.java
+++ app/src/main/java/com/example/demouser/motrade/AddItemActivity.java
@@ -1,15 +1,23 @@
 package com.example.demouser.motrade;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class AddItemActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private ImageButton picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,12 +31,33 @@ public class AddItemActivity extends AppCompatActivity {
         //get button
         Button postButton = (Button) findViewById(R.id.addButton);
 
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            picture = findViewById(R.id.itemIconUploader);
+            picture.setEnabled(false);
+        }
+
         //set button listener
         postButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
             }
         });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            picture.setImageBitmap(imageBitmap);
+        }
     }
 
 }
